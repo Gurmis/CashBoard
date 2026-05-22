767b265385d6b98700ca23863c73d84347f197b2
Peter Girman <peter.girman@websupport.eu>
2026-05-22 15:48:43 +0200

    moved nav-item styles to the component

--- src/components/ui/NavItem.vue
+++ src/components/ui/NavItem.vue
@@ -26,9 +26,48 @@ const props = withDefaults(defineProps<{
 </script>
 
 <style scoped lang="scss">
-.nav-item__content {
+.nav-item {
   display: flex;
   align-items: center;
-  gap: 8px;
+  color: var(--nav-item-color);
+  /*border: 1px solid transparent;*/
+  border-radius: var(--radius-xl);
+  font-weight: 500;
+  text-decoration: none;
+  transition:
+      color var(--transition-fast);
+
+  &:hover {
+    background: var(--nav-item-hover-bg);
+    border-color: var(--nav-item-hover-border);
+    box-shadow: var(--nav-item-hover-shadow);
+    backdrop-filter: blur(1px);
+    -webkit-backdrop-filter: blur(1px);
+    transform: translateY(-2px);
+  }
+
+  &--active {
+    background: var(--nav-item-active-bg);
+    color: var(--nav-item-active-color);
+    border-color: var(--nav-item-active-border);
+    box-shadow: var(--nav-item-active-shadow);
+    font-weight: 600;
+    backdrop-filter: blur(1px);
+    -webkit-backdrop-filter: blur(1px);
+
+    &:hover {
+      background: var(--nav-item-active-bg);
+      border-color: var(--nav-item-active-border);
+      box-shadow: var(--nav-item-active-shadow);
+      transform: translateY(0);
+    }
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
 }
+
 </style>
