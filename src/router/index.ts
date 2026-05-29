9ca2607e0438b91874dcc89f6e5fef6f6ccec747
Peter Girman <peter.girman@websupport.eu>
2026-05-29 14:09:58 +0200

    restructure routes with layouts and additional views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,39 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import DashboardView from "../view/DashboardView.vue";
-import ExpensesView from "@/view/ExpensesView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from "@/views/DashboardView.vue";
+import ExpensesView from "@/views/ExpensesView.vue";
+import AuthLayout from "@/components/layout/AuthLayout.vue";
+import AppLayout from "@/components/layout/AppLayout.vue";
+import LoginView from "@/views/LoginView.vue";
+import ReportsView from "@/views/ReportsView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+
+const appRoutes = [
+    { name: 'dashboard', path: '/dashboard', component: DashboardView },
+    { name: 'expenses', path: '/expenses', component: ExpensesView },
+    { name: 'reports', path: '/reports', component: ReportsView },
+    { name: 'settings', path: '/settings', component: SettingsView },
+]
+
+const authRoutes = [
+    { path: '', component: LoginView }
+]
 
 const routes = [
-    { path: '/', component: DashboardView },
-    { path: '/expenses', component: ExpensesView },
+    {
+        path: '/',
+        component: AppLayout,
+        children: [
+            { path: '', redirect: {name: 'dashboard'}},
+            ...appRoutes
+        ]
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: AuthLayout,
+        children: authRoutes
+    },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
