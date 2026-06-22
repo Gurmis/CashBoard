fef457d490ffbbac1330fd33b6d10700ca2e7825
Peter Girman <peter.girman@websupport.eu>
2026-06-22 10:25:15 +0200

    feat: add computed property for statCards in useExpenses composable

--- src/composables/useExpenses.ts
+++ src/composables/useExpenses.ts
@@ -1,18 +1,23 @@
-import { ref } from "vue";
+import {computed, ref} from "vue";
 import type { Expense, ExpenseSummary } from "@/features/expenses/types.ts";
 import type {StatCardConfig} from "@/types.ts";
+import StatCard from "@/components/ui/StatCard.vue";
 
 export const useExpenses = () => {
-const summary = ref<ExpenseSummary[]>([]);
-const statCards = ref<StatCardConfig[]>([]);
+const expenseSummary = ref<ExpenseSummary[]>([]);
 const expenses = ref<Expense[]>([]);
 const isLoading = ref<boolean>(false);
 const error = ref<string | null>(null);
 const loadData = async () => {};
 
+const statCards = computed<StatCardConfig[]>(() => {
+    // TODO mapping
+    return [];
+})
+
 return {
-    statCards,
     expenses,
+    statCards,
     isLoading,
     error,
     loadData
